79afa53c6c00f6209645276397e4118bbfe117b9
grihanvskn <gvoskanov7@gmail.com>
2025-07-09 23:04:31 +0300

    board and dice
    added a board and a placeholder dice rolling mechanism

---
+++ Assets/Scripts/Game/DiceRoller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DiceRoller : MonoBehaviour
+{
+    //временное отражение кубика текстом
+    public TMP_Text Die1;
+    public TMP_Text Die2;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //функция которая вызывается по нажатию кнопки roll
+    public void Roll()
+    {
+        //бросаем кости и меняем текст
+        int a = Random.Range(1, 7);
+        int b = Random.Range(1, 7);
+
+        Die1.text = a.ToString();
+        Die2.text = b.ToString();
+    }
+}
